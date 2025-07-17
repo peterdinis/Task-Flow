acdac8e883ddacd9d6f94c58370447435f78c1d7
peterdinis <petter1@pobox.sk>
2025-07-17 17:06:44 +0200

    feat: Setup hono

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,18 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import type { PageConfig } from 'next'
+
+export const config: PageConfig = {
+  runtime: 'edge',
+}
+
+const app = new Hono().basePath('/api')
+
+app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello Next.js!',
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
