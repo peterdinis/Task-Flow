17dd09dce463b12ad3f7655d1874c751416f55a0
peterdinis <petter1@pobox.sk>
2025-07-17 12:59:42 +0200

    feat: Finish landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
+import CTASection from "@/components/hero/CTASection";
 import FeaturesSection from "@/components/hero/FeaturesSection";
 import HeroSection from "@/components/hero/HeroSection";
 import StatsSection from "@/components/hero/StatsSection";
+import TestimonialSection from "@/components/hero/TestimonialSection";
+import Footer from "@/components/shared/Footer";
 import { NextPage } from "next";
 
 const Homepage: NextPage = () => {
   return (
     <>
-        <HeroSection />
-        <StatsSection />
-        <FeaturesSection />
+      <HeroSection />
+      <StatsSection />
+      <FeaturesSection />
+      <TestimonialSection />
+      <CTASection />
+      <Footer />
     </>
   )
 }
