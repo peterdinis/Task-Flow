7078a0896bfc62ddfe3a8ea02783f649945eb51b
peterdinis <petter1@pobox.sk>
2025-07-17 17:37:29 +0200

    feat: Adding react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import Navigation from "@/components/shared/Navigation";
 import ScrollToTop from "@/components/shared/ScrollToTop";
+import QueryProvider from "@/components/providers/QueryProvider";
 
 const ubuntu = Ubuntu({
 	weight: "500",
@@ -31,9 +32,11 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
-					<Navigation />
-					{children}
-					<ScrollToTop />
+					<QueryProvider>
+						<Navigation />
+						{children}
+						<ScrollToTop />
+					</QueryProvider>
 				</ThemeProvider>
 			</body>
 		</html>
