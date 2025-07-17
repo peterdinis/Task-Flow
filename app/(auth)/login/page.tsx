ef72552a751808f9721ea8899046aeb494357d95
peterdinis <petter1@pobox.sk>
2025-07-17 13:11:38 +0200

    FOO

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const LoginPage: NextPage = () => {
+    return (
+        <></>
+    )
+}
+
+export default LoginPage
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const RegisterPage: NextPage = () => {
+    return (
+        <></>
+    )
+}
+
+export default RegisterPage
