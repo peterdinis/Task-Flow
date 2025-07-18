6e1b23bd640cbd2312a3c81a2a6597f003b47d25
peterdinis <petter1@pobox.sk>
2025-07-18 18:23:20 +0200

    FOO

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,7 +1,21 @@
-import app from "@/server";
-import { handle } from "hono/vercel";
+import { Hono } from "hono";
+import {handle} from "hono/vercel"
+import authRoutes from "../auth/route";
+import boardRoutes from "../boards/route";
 
-export const GET = handle(app);
-export const POST = handle(app);
-export const PUT = handle(app);
-export const DELETE = handle(app);
+export const runtime = "edge"
+
+const app = new Hono().basePath("/api")
+
+const routes = app
+.route("/auth", authRoutes)
+.route("/boards", boardRoutes)
+
+
+export const GET = handle(app)
+export const POST = handle(app)
+export const PATCH = handle(app)
+export const PUT = handle(app)
+export const DELETE = handle(app)
+
+export type AppType = typeof routes
