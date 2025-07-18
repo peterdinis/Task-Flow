963afa48fc5f17d1f219c2ab84a4cb6a89f9e432
peterdinis <petter1@pobox.sk>
2025-07-18 18:23:58 +0200

    feat: Formatting files

--- schemas/boardSchema.ts
+++ schemas/boardSchema.ts
@@ -1,9 +1,9 @@
-import {z} from "zod"
+import { z } from "zod";
 
 export const boardSchema = z.object({
-  title: z.string().min(1),
-  description: z.string().min(1),
-  progress: z.number(),
-  projectColor: z.string(),
-  ownerId: z.string(),
-})
+	title: z.string().min(1),
+	description: z.string().min(1),
+	progress: z.number(),
+	projectColor: z.string(),
+	ownerId: z.string(),
+});
