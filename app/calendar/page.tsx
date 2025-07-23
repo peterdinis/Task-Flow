e761e521daf5a9a86b58ced5fe1a7c00350310a1
peterdinis <petter1@pobox.sk>
2025-07-23 18:10:53 +0200

    feat: Finish pages

---
+++ app/calendar/page.tsx
@@ -0,0 +1,8 @@
+import CalendarWrapper from "@/components/calendar/CalendarWrapper";
+import { NextPage } from "next";
+
+const CalendarPage: NextPage = () => {
+    return <CalendarWrapper />
+}
+
+export default CalendarPage;
