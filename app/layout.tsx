29a73b7dd8367e53cf19ffbc66a0e61097dc42cc
peterdinis <petter1@pobox.sk>
2025-07-23 16:02:13 +0200

    feat: Adding dark mode WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ScrollToTopButton } from "@/components/shared/ScrollToTop";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <ScrollToTopButton />
+        <ThemeProvider 
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          {children}
+          <ScrollToTopButton />
+        </ThemeProvider>
       </body>
     </html>
   );
