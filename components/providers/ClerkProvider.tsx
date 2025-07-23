71b9e7d067fb2364ed124ebe20e32eade4b75bac
peterdinis <petter1@pobox.sk>
2025-07-23 16:40:47 +0200

    feat: Start adding clerk

---
+++ components/providers/ClerkProvider.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { FC, ReactNode } from "react";
+
+type ClerkProviderProps = {
+    children: ReactNode;
+};
+
+const AppClerkProvider: FC<ClerkProviderProps> = ({children}: ClerkProviderProps) => {
+    return (
+        <ClerkProvider>
+            {children}
+        </ClerkProvider>
+    )
+}
+
+export default AppClerkProvider;
